473635d075d7005e81dde68f12c1af0aff55f264
kbj1126 <59276712+kbj1126@users.noreply.github.com>
2020-01-03 01:34:47 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,101 @@
+
+var imageSlideIndex = 1;
+showImageSlides(imageSlideIndex);
+
+function imageSlideTimer() {
+  plusImageSlides(1);
+}
+//////////////////
+var imageTimer = setInterval(imageSlideTimer, 3000);
+
+function plusImageSlides(n) {
+  clearInterval(imageTimer);
+  imageTimer = setInterval(imageSlideTimer, 3000);
+  showImageSlides(imageSlideIndex += n);
+}
+///////////////////////////
+function currentImageSlide(n){
+  clearInterval(imageTimer);
+  imageTimer = setInterval(imageSlideTimer, 3000);
+
+  showImageSlides(imageSlideIndex = n);
+}
+/////////////////////////////
+function showImageSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName('image-slide');
+  var dots = document.getElementsByClassName('dot');
+  if (n > slides.length) {imageSlideIndex = 1}
+  if (n < 1) {imageSlideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'none';
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(' active', '');
+  }
+  slides[imageSlideIndex-1].style.display = 'block';
+  dots[imageSlideIndex-1].className += ' active';
+}
+//////////////////////
+document.getElementById('imagePrev').addEventListener('click', plusImageSlides.bind(null,-1));
+document.getElementById('imageNext').addEventListener('click', plusImageSlides.bind(null,1));
+
+document.getElementById('firstDot').addEventListener('click', currentImageSlide.bind(null,1));
+document.getElementById('secondDot').addEventListener('click', currentImageSlide.bind(null,2));
+document.getElementById('thirdDot').addEventListener('click', currentImageSlide.bind(null,3));
+document.getElementById('forthDot').addEventListener('click', currentImageSlide.bind(null,4));
+
+
+
+
+function addClass(element, name) {
+    if(element.className.indexOf(name) == -1) {
+      element.className += " " + name;
+    }
+}
+//////////////////////////////
+function removeClass(element, name) {
+  var arr;
+  arr = element.className.split(" ");
+
+ while(arr.indexOf(name) > -1){
+   arr.splice(arr.indexOf(name), 1);
+ }
+
+ element.className = arr.join(" ");
+}
+/////////////////////////////////////////////////////
+
+var reviewSlideIndex = 0;
+
+function reviewSlideTimer() {
+  plusReviewSlides(1);
+}
+/////////////////////////
+var reviewTimer = setInterval(reviewSlideTimer, 3000);
+
+function showReviewSlides(n) {
+  var i;
+  var review_slides = document.getElementsByClassName('review-slide');
+
+  if (n > review_slides.length - 3) {
+    reviewSlideIndex = 0;
+  }
+
+  if (n < 0) {
+    reviewSlideIndex = review_slides.length - 3;
+  }
+
+  for (i = 0; i < review_slides.length; i++) {
+    removeClass(review_slides[i], 'show');
+    removeClass(review_slides[i], 'res-show');
+    addClass(review_slides[i], 'hide');
+  }
+
+  removeClass(review_slides[reviewSlideIndex], 'hide');
+  addClass(review_slides[reviewSlideIndex], 'res-show');
+  removeClass(review_slides[reviewSlideIndex+1], 'hide');
+  addClass(review_slides[reviewSlideIndex+1], 'show');
+  removeClass(review_slides[reviewSlideIndex+2], 'hide');
+  addClass(review_slides[reviewSlideIndex+2], 'show');
+}
---
+++ sub2.js
@@ -0,0 +1,42 @@
+var list = [document.getElementById("listmenu1"),document.getElementById("listmenu2"),document.getElementById("listmenu3"),document.getElementById("listmenu4"),document.getElementById("listmenu5")];
+var item = [document.getElementById("listitem1"),document.getElementById("listitem2"),document.getElementById("listitem3"),document.getElementById("listitem4"),document.getElementById("listitem5")]
+list[0].addEventListener('click',function(event){
+  for(var i=0;i<5;i++){
+    list[i].style.backgroundColor = "#d3d3d3";
+    item[i].style.display='none';
+  }
+  this.style.backgroundColor="#6e6e6e";
+  item[0].style.display='block';
+});
+list[1].addEventListener('click',function(event){
+  for(var i=0;i<5;i++){
+    list[i].style.backgroundColor = "#d3d3d3";
+    item[i].style.display='none';
+  }
+  this.style.backgroundColor="#6e6e6e";
+  item[1].style.display='block';
+});
+list[2].addEventListener('click',function(event){
+  for(var i=0;i<5;i++){
+    list[i].style.backgroundColor = "#d3d3d3";
+    item[i].style.display='none';
+  }
+  this.style.backgroundColor="#6e6e6e";
+  item[2].style.display='block';
+});
+list[3].addEventListener('click',function(event){
+  for(var i=0;i<5;i++){
+    list[i].style.backgroundColor = "#d3d3d3";
+    item[i].style.display='none';
+  }
+  this.style.backgroundColor="#6e6e6e";
+  item[3].style.display='block';
+});
+list[4].addEventListener('click',function(event){
+  for(var i=0;i<5;i++){
+    list[i].style.backgroundColor = "#d3d3d3";
+    item[i].style.display='none';
+  }
+  this.style.backgroundColor="#6e6e6e";
+  item[4].style.display='block';
+});
